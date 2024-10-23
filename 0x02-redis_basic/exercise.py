6fa1075a3b441db440124265a7407143c4283684
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-10-23 21:00:48 +0300

    done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+
+import redis
+import uuid
+class Cache():
+
+    def __init__(self):
+        _redis = redis.Redis()
+        _redis.flushdb()
+
+    def store(self, data) -> str:
+        key = uuid.uuid4()
+        self._redis.set(key, data)
+        return key
