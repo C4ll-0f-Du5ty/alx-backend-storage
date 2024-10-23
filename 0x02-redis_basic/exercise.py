0e4de57ea6a9d51262e504d7e5a6ff3e7373f447
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-10-23 21:25:57 +0300

    done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -24,3 +24,37 @@ class Cache():
         key: str = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+
+    def get(self, key: str, fn: typing.Optional[callable] = None) -> typing.Union[str, bytes, int, float]:
+        """_summary_
+
+        Args:
+            key (str): _description_
+            fn (typing.Optional[callable], optional): _description_. Defaults to None.
+
+        Returns:
+            typing.Union[str, bytes, int, float]: _description_
+        """
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+
+    def get_str(self, key: str) -> str:
+        """automatically parametrize Cache.get with the correct
+        conversion function"""
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+
+    def get_int(self, key: str) -> int:
+        """automatically parametrize Cache.get with the correct
+        conversion function"""
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except Exception:
+            value = 0
+        return value
